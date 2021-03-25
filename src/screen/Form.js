90493ea033d795a3ad6d88b2e0d244ec58aafd9a
EhsanShakil <ahsanshakeel1994@gmail.com>
2021-03-25 23:53:13 +0500

    Updating

--- src/screen/Form.js
+++ src/screen/Form.js
@@ -1,55 +1,68 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {View, Text, Button, TextInput} from 'react-native';
 import {Formik, Field} from 'formik';
 import * as Yup from 'yup';
 
 const FormikForm = () => {
-  const initialValues = {
-    fullName: '',
-    lastName: '',
-    email: '',
-    password: '',
-  };
-  const nameSchema = Yup.object().shape({
-    fullName: Yup.string().required(),
-    lastName: Yup.string().required(),
-    email: Yup.number().required(),
-    password: Yup.string().required(),
-  });
-  const onSubmit = values => {
-    alert('Form Submitted Successfully!!!');
-    console.log(values);
-  };
   return (
     <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={nameSchema}>
-      {({values, handleChange, handleSubmit, isValid}) => (
-        <>
+      initialValues={{firstName: '', lastName: '', email: '', password: ''}}
+      onSubmit={values => alert(JSON.stringify(values))}
+      validationSchema={Yup.object().shape({
+        firstName: Yup.string().required('first name is required'),
+        lastName: Yup.string().required('last name is required'),
+        email: Yup.string().required('email is required'),
+        password: Yup.string().required('password is required'),
+      })}>
+      {({
+        values,
+        handleChange,
+        errors,
+        setFieldTouched,
+        touched,
+        isValid,
+        handleSubmit,
+      }) => (
+        <Fragment>
           <TextInput
-            value={values.fullName}
-            onChangeText={handleChange('fullName')}
-            placeholder="Full Name"
+            value={values.firstName}
+            onChangeText={handleChange('firstName')}
+            onBlur={() => setFieldTouched('firstName')}
+            placeholder="First Name"
           />
+          {touched.firstName && errors.firstName && (
+            <Text style={{fontSize: 10, color: 'red'}}>{errors.firstName}</Text>
+          )}
           <TextInput
             value={values.lastName}
             onChangeText={handleChange('lastName')}
+            onBlur={() => setFieldTouched('lastName')}
             placeholder="Last Name"
           />
+          {touched.lastName && errors.lastName && (
+            <Text style={{fontSize: 10, color: 'red'}}>{errors.lastName}</Text>
+          )}
           <TextInput
             value={values.email}
             onChangeText={handleChange('email')}
-            placeholder="Email"
+            onBlur={() => setFieldTouched('email')}
+            placeholder="E-mail"
           />
+          {touched.email && errors.email && (
+            <Text style={{fontSize: 10, color: 'red'}}>{errors.email}</Text>
+          )}
           <TextInput
             value={values.password}
             onChangeText={handleChange('password')}
             placeholder="Password"
-            secureTextEntry
+            onBlur={() => setFieldTouched('password')}
+            secureTextEntry={true}
           />
-          <Button title="SIGN UP" disabled={!isValid} onPress={handleSubmit} />
-        </>
+          {touched.password && errors.password && (
+            <Text style={{fontSize: 10, color: 'red'}}>{errors.password}</Text>
+          )}
+          <Button title="Sign In" disabled={!isValid} onPress={handleSubmit} />
+        </Fragment>
       )}
     </Formik>
   );
